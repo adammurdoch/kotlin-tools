8eb41135c7ea3f7375a4fae870aad8559014d62a
Adam Murdoch <adam@gradle.com>
2022-10-02 07:02:47 +1100

    Tweaks to native binary generation.

--- launcher-plugins/launcher-plugins/src/main/kotlin/net/rubygrapefruit/app/tasks/NativeBinary.kt
+++ launcher-plugins/launcher-plugins/src/main/kotlin/net/rubygrapefruit/app/tasks/NativeBinary.kt
@@ -1,6 +1,6 @@
 package net.rubygrapefruit.app.tasks
 
-import net.rubygrapefruit.app.internal.Linux
+import net.rubygrapefruit.app.NativeMachine
 import net.rubygrapefruit.app.internal.currentOs
 import net.rubygrapefruit.download.DownloadRepository
 import org.gradle.api.DefaultTask
@@ -34,35 +34,41 @@ abstract class NativeBinary : DefaultTask() {
     @TaskAction
     fun install() {
         val repository = DownloadRepository()
-        val uri = if (currentOs is Linux) {
-            URI("https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.2.0/graalvm-ce-java11-linux-amd64-22.2.0.tar.gz")
-        } else {
-            URI("https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.2.0/graalvm-ce-java11-darwin-amd64-22.2.0.tar.gz")
-        }
-        val binDirName = if (currentOs is Linux) {
-            "graalvm-ce-java11-22.2.0/bin"
-        } else {
-            "graalvm-ce-java11-22.2.0/Contents/Home/bin"
+        val args = when (currentOs.machine) {
+            NativeMachine.LinuxX64 -> Args(
+                URI("https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.2.0/graalvm-ce-java11-linux-amd64-22.2.0.tar.gz"),
+                "graalvm-ce-java11-linux-amd64-22.2.0",
+                "graalvm-ce-java11-22.2.0/bin",
+                "linux-amd64"
+            )
+            NativeMachine.MacOSX64, NativeMachine.MacOSArm64 -> Args(
+                URI("https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.2.0/graalvm-ce-java11-darwin-amd64-22.2.0.tar.gz"),
+                "graalvm-ce-java11-darwin-amd64-22.2.0",
+                "graalvm-ce-java11-22.2.0/Contents/Home/bin",
+                "darwin-amd64"
+            )
+            else -> TODO()
         }
 
-        val dir = repository.install(uri, "graalvm-amd64-22.2") { dir ->
-            val tool = dir.resolve("$binDirName/gu")
+        val dir = repository.install(args.distribution, args.installName) { dir ->
+            val tool = dir.resolve("${args.binDirPath}/gu")
             processOperations.exec { spec ->
                 spec.commandLine(tool, "install", "native-image")
             }
         }
 
-        val nativeImage = dir.resolve("$binDirName/native-image")
+        val nativeImage = dir.resolve("${args.binDirPath}/native-image")
         processOperations.exec { spec ->
             spec.commandLine(
                 nativeImage,
-                "-o",
-                launcherFile.get().asFile.absolutePath,
-                "--module-path",
-                modulePath.asPath,
-                "--module",
-                "${module.get()}/${mainClass.get()}"
+                "-o", launcherFile.get().asFile.absolutePath,
+                "--target=${args.target}",
+                "--no-fallback",
+                "--module-path", modulePath.asPath,
+                "--module", "${module.get()}/${mainClass.get()}"
             )
         }
     }
+
+    private class Args(val distribution: URI, val installName: String, val binDirPath: String, val target: String)
 }

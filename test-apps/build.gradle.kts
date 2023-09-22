8399ed709ca8ee76446fea938f0ca0bb2479f716
Adam Murdoch <adam@gradle.com>
2023-09-23 07:09:27 +1000

    More sample generation.

--- test-apps/build.gradle.kts
+++ test-apps/build.gradle.kts
@@ -1,6 +1,6 @@
 import java.io.PrintWriter
 
-sealed class Sample(val name: String, val baseDir: File, val srcDirName: String = "main") {
+sealed class Sample(val name: String, val baseDir: File, val srcDirName: String) {
     val dir = baseDir.resolve(name)
 
     val srcDir = dir.resolve("src/$srcDirName/kotlin")
@@ -14,9 +14,9 @@ interface DerivedSample {
     val srcDir: File
 }
 
-sealed class App(name: String, baseDir: File, val launcher: String) : Sample(name, baseDir)
+sealed class App(name: String, baseDir: File, val launcher: String, srcDirName: String) : Sample(name, baseDir, srcDirName)
 
-class BaseApp(name: String, baseDir: File, launcher: String) : App(name, baseDir, launcher) {
+class BaseApp(name: String, baseDir: File, launcher: String, srcDirName: String) : App(name, baseDir, launcher, srcDirName) {
     fun derive(suffix: String, launcher: String? = null): DerivedApp {
         val sampleName = "$name-$suffix"
         return DerivedApp(sampleName, this, baseDir, launcher ?: sampleName)
@@ -24,7 +24,7 @@ class BaseApp(name: String, baseDir: File, launcher: String) : App(name, baseDir
 }
 
 class DerivedApp(name: String, override val derivedFrom: BaseApp, baseDir: File, launcher: String) :
-    App(name, baseDir, launcher), DerivedSample
+    App(name, baseDir, launcher, derivedFrom.srcDirName), DerivedSample
 
 class BaseLib(name: String, baseDir: File, srcDirName: String) : Sample(name, baseDir, srcDirName) {
     fun derive(suffix: String): DerivedLib {
@@ -40,6 +40,8 @@ val jvmCliApp = app("jvm-cli-app")
 val jvmUiApp = app("jvm-ui-app")
 val jvmLib = lib("jvm-lib")
 val mppLib = mppLib("mpp-lib")
+val nativeCliApp = app("native-cli-app")
+val nativeUiApp = macOsApp("native-ui-app")
 
 val samples = listOf(
     jvmLib,
@@ -57,9 +59,18 @@ val samples = listOf(
 
     jvmUiApp,
     jvmUiApp.derive("customized"),
+
+    nativeCliApp,
+
+    nativeUiApp,
+    nativeUiApp.derive("customized")
 )
 
 val generators = samples.filterIsInstance<DerivedSample>().map { sample ->
+    if (!sample.derivedFrom.srcDir.isDirectory) {
+        throw IllegalArgumentException("Missing source directory ${sample.derivedFrom.srcDir}")
+    }
+
     tasks.register("generate-${sample.name}") {
         doLast {
             sync {
@@ -90,7 +101,9 @@ tasks.register("generate") {
     dependsOn(generators, script)
 }
 
-fun app(name: String) = BaseApp(name, projectDir, name)
+fun app(name: String) = BaseApp(name, projectDir, name, "main")
+
+fun macOsApp(name: String) = BaseApp(name, projectDir, name, "macosMain")
 
 fun lib(name: String) = BaseLib(name, projectDir, "main")
 

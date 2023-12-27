fe6c02fdfd893608c3c1ae5b1669decdace4bd67
Adam Murdoch <adam@gradle.com>
2023-12-28 08:18:29 +1100

    Implement test apps.

--- test-apps/jvm-cli-app-full/src/main/kotlin/sample/Main.kt
+++ test-apps/jvm-cli-app-full/src/main/kotlin/sample/Main.kt
@@ -1,5 +1,31 @@
 package sample
 
-fun main(args: Array<String>) {
-    println("Hello world")
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.multiple
+
+class TodoApp : CliktCommand() {
+    init {
+        subcommands(ListCommand(), AddCommand())
+    }
+
+    override fun run() {
+    }
 }
+
+class ListCommand : CliktCommand(name = "list") {
+    override fun run() {
+        println("list entries")
+    }
+}
+
+class AddCommand : CliktCommand(name = "add") {
+    private val entry by argument().multiple()
+
+    override fun run() {
+        println("add entry: $entry")
+    }
+}
+
+fun main(args: Array<String>) = TodoApp().main(args)
--- test-apps/native-cli-app-full/build.gradle.kts
+++ test-apps/native-cli-app-full/build.gradle.kts
@@ -1,3 +1,9 @@
 plugins {
     id("net.rubygrapefruit.native.cli-app")
 }
+
+application {
+    common {
+        implementation("com.github.ajalt.clikt:clikt:4.2.1")
+    }
+}

49c322b12ff548dda1ac9143e8a2b6a1c17c137f
Adam Murdoch <adam@gradle.com>
2023-12-28 07:40:37 +1100

    Fixes for automatic modules.

--- base-plugins/src/main/kotlin/net/rubygrapefruit/plugins/app/internal/tasks/RuntimeModulePath.kt
+++ base-plugins/src/main/kotlin/net/rubygrapefruit/plugins/app/internal/tasks/RuntimeModulePath.kt
@@ -5,7 +5,7 @@ package net.rubygrapefruit.plugins.app.internal.tasks
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
-import net.rubygrapefruit.bytecode.BytecodeWriter
+import net.rubygrapefruit.bytecode.*
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.DirectoryProperty
@@ -15,6 +15,8 @@ import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
 import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
 import java.util.zip.ZipOutputStream
@@ -39,13 +41,14 @@ abstract class RuntimeModulePath : DefaultTask() {
         val modulesByFile = (modules.requires + modules.transitive).associateBy { it.fileName }
 
         val bytecodeWriter = BytecodeWriter()
+        val bytecodeReader = BytecodeReader()
 
         for (file in classpath) {
             val module = modulesByFile.getValue(file.name)
             val dest = dir.resolve(file.name)
             if (module.automatic) {
                 println("-> NEED TO PATCH ${file.name}")
-                patchJarFile(file, dest, bytecodeWriter, module)
+                patchJarFile(file, dest, bytecodeReader, bytecodeWriter, module)
             } else {
                 println("-> DO NOT PATCH ${file.name}")
                 file.copyTo(dest)
@@ -53,9 +56,10 @@ abstract class RuntimeModulePath : DefaultTask() {
         }
     }
 
-    private fun patchJarFile(file: File, dest: File, bytecodeWriter: BytecodeWriter, module: InferredModule) {
+    private fun patchJarFile(file: File, dest: File, bytecodeReader: BytecodeReader, bytecodeWriter: BytecodeWriter, module: InferredModule) {
         file.inputStream().use { instr ->
             val zipInstr = ZipInputStream(instr)
+            val packages = mutableSetOf<String>()
             dest.outputStream().use { outstr ->
                 val zipOutstr = ZipOutputStream(outstr)
                 while (true) {
@@ -64,15 +68,55 @@ abstract class RuntimeModulePath : DefaultTask() {
                         break
                     }
                     zipOutstr.putNextEntry(entry)
-                    zipInstr.copyTo(zipOutstr)
+                    if (entry.name.endsWith(".class")) {
+                        val stream = CopyingInputStream(zipInstr, zipOutstr)
+                        bytecodeReader.readFrom(stream, object : ClassFileVisitor {
+                            override fun type(type: TypeInfo): TypeVisitor? {
+                                val packageName = type.name.substringBeforeLast(".", "")
+                                if (packageName.isNotEmpty()) {
+                                    packages.add(packageName)
+                                }
+                                return null
+                            }
+                        })
+                        stream.finish()
+                    } else {
+                        zipInstr.copyTo(zipOutstr)
+                    }
                     zipOutstr.closeEntry()
                 }
                 zipOutstr.putNextEntry(ZipEntry("module-info.class"))
+                println("-> EXPORTS: $packages")
                 bytecodeWriter.writeTo(zipOutstr) {
-                    module(module.name, emptyList(), emptyList(), emptyList())
+                    module(module.name, packages.toList(), emptyList(), emptyList())
                 }
                 zipOutstr.closeEntry()
                 zipOutstr.finish()
+            }
+        }
+    }
+
+    private class CopyingInputStream(val inputStream: InputStream, val outputStream: OutputStream) : InputStream() {
+        override fun read(): Int {
+            val b = inputStream.read()
+            if (b >= 0) {
+                outputStream.write(b)
+            }
+            return b
+        }
+
+        override fun read(buffer: ByteArray, offset: Int, count: Int): Int {
+            val nread = inputStream.read(buffer, offset, count)
+            if (nread >= 0) {
+                outputStream.write(buffer, offset, nread)
+            }
+            return nread
+        }
+
+        fun finish() {
+            val buffer = ByteArray(4096)
+            while (read(buffer) >= 0) {
+                //
             }
         }
     }

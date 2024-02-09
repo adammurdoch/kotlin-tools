5dfc243e2e7c9eb0c7e657bfcba240294cbb23cb
Adam Murdoch <adam@gradle.com>
2024-02-10 05:24:39 +1100

    Add some test coverage.

--- libs/store/src/commonTest/kotlin/net/rubygrapefruit/store/KeyValueStoreTest.kt
+++ libs/store/src/commonTest/kotlin/net/rubygrapefruit/store/KeyValueStoreTest.kt
@@ -1,6 +1,7 @@
 package net.rubygrapefruit.store
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
@@ -12,6 +13,7 @@ class KeyValueStoreTest : AbstractStoreTest() {
 
             val value = store.keyValue<String, Int>("value")
             assertNull(value.get("a"))
+            assertTrue(value.entries().isEmpty())
 
             // Not visible until it has been written to
             assertTrue(store.values().isEmpty())
@@ -19,9 +21,55 @@ class KeyValueStoreTest : AbstractStoreTest() {
 
         withStore { store ->
             assertTrue(store.values().isEmpty())
+
             val value = store.keyValue<String, Int>("value")
             assertNull(value.get("a"))
+            assertTrue(value.entries().isEmpty())
+
             assertTrue(store.values().isEmpty())
+        }
+    }
+
+    @Test
+    fun `can read from then write to store`() {
+        withStore { store ->
+            val value = store.keyValue<String, Int>("value")
+            assertNull(value.get("a"))
+
+            value.set("a", 12)
+            assertEquals(12, value.get("a"))
+            assertEquals(listOf("a" to 12), value.entries())
+
+            assertEquals(listOf("value"), store.values())
+        }
+
+        withStore { store ->
+            assertEquals(listOf("value"), store.values())
+
+            val value = store.keyValue<String, Int>("value")
+            assertEquals(12, value.get("a"))
+            assertEquals(listOf("a" to 12), value.entries())
+        }
+    }
+
+    @Test
+    fun `can write to then read from store`() {
+        withStore { store ->
+            val value = store.keyValue<String, Int>("value")
+
+            value.set("a", 12)
+            assertEquals(12, value.get("a"))
+            assertEquals(listOf("a" to 12), value.entries())
+
+            assertEquals(listOf("value"), store.values())
+        }
+
+        withStore { store ->
+            assertEquals(listOf("value"), store.values())
+
+            val value = store.keyValue<String, Int>("value")
+            assertEquals(12, value.get("a"))
+            assertEquals(listOf("a" to 12), value.entries())
         }
     }
 }

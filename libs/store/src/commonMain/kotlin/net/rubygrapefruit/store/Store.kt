538c057dca6bf592a4d918de565761ed111586d9
Adam Murdoch <adam@gradle.com>
2024-02-24 07:57:43 +1100

    Javadocs.

--- libs/store/src/commonMain/kotlin/net/rubygrapefruit/store/Store.kt
+++ libs/store/src/commonMain/kotlin/net/rubygrapefruit/store/Store.kt
@@ -8,6 +8,14 @@ import net.rubygrapefruit.file.Directory
 import net.rubygrapefruit.file.RegularFile
 import net.rubygrapefruit.io.codec.SimpleCodec
 
+/**
+ * A [Store] is a persistent container of data stored in a single directory.
+ *
+ * A store can contain one or more of the following entries:
+ *
+ * - [ValueStore]: A mutable value of type T.
+ * - [KeyValueStore]: A mutable map with keys of type K and values of type T.
+ */
 class Store private constructor(
     indexFile: RegularFile,
     dataFile: RegularFile
@@ -17,6 +25,9 @@ class Store private constructor(
     private val data = DataFile(dataFile, codec)
 
     companion object {
+        /**
+         * Opens the store in the given directory.
+         */
         fun open(directory: Directory, discard: Boolean = false): Store {
             directory.createDirectories()
             val index = directory.file("index.bin")
@@ -29,22 +40,37 @@ class Store private constructor(
         }
     }
 
+    /**
+     * Opens the [ValueStore] with the given name and type, creating it if it does not exist.
+     */
     fun <T : Any> value(name: String, serializer: KSerializer<T>): ValueStore<T> {
         return DefaultValueStore(name, index, data, serializer)
     }
 
+    /**
+     * Opens the [ValueStore] with the given name and type, creating it if it does not exist.
+     */
     inline fun <reified T : Any> value(name: String): ValueStore<T> {
         return value(name, serializer())
     }
 
+    /**
+     * Opens the [KeyValueStore] with the given name and type, creating it if it does not exist.
+     */
     fun <K : Any, V : Any> keyValue(name: String, keySerializer: KSerializer<K>, valueSerializer: KSerializer<V>): KeyValueStore<K, V> {
         return DefaultKeyValueStore(name, index, data, keySerializer, valueSerializer)
     }
 
+    /**
+     * Opens the [KeyValueStore] with the given name and type, creating it if it does not exist.
+     */
     inline fun <reified K : Any, reified V : Any> keyValue(name: String): KeyValueStore<K, V> {
         return keyValue(name, serializer(), serializer())
     }
 
+    /**
+     * Visits a summary of the content of this store.
+     */
     fun accept(visitor: ContentVisitor) {
         index.accept(visitor)
     }

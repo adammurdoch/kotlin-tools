e577a7729b418577181eee7f917f1a95784dfeb7
Adam Murdoch <adam@gradle.com>
2024-06-10 06:47:35 +1000

    Update test apps.

--- test-apps/jvm-lib-customized/src/main/kotlin/sample/system/SystemInfo.kt
+++ test-apps/jvm-lib-customized/src/main/kotlin/sample/system/SystemInfo.kt
@@ -1,7 +1,8 @@
 package sample.system
 
 fun reportSystemInfo() {
-    println("Java: ${System.getProperty("java.version")} (${System.getProperty("java.vendor")})")
+    println("Kotlin: ${KotlinVersion.CURRENT}")
+    println("JVM: ${System.getProperty("java.version")} (${System.getProperty("java.vendor")})")
     println("OS: ${System.getProperty("os.name")} (${System.getProperty("os.version")} ${System.getProperty("os.arch")})")
     println()
 }
--- test-apps/native-lib/src/commonMain/kotlin/sample/system/SystemInfo.kt
+++ test-apps/native-lib/src/commonMain/kotlin/sample/system/SystemInfo.kt
@@ -4,6 +4,7 @@ import kotlin.experimental.ExperimentalNativeApi
 
 @OptIn(ExperimentalNativeApi::class)
 fun reportSystemInfo() {
+    println("Kotlin: ${KotlinVersion.CURRENT}")
     println("OS: ${Platform.osFamily} arch ${Platform.cpuArchitecture}")
     println()
 }

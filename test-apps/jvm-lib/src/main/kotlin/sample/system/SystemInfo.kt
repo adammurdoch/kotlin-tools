179a3bc62af5d0a8785678e02cd7aa9b88a74c0f
Adam Murdoch <adam@gradle.com>
2024-09-13 18:05:29 +1000

    Tweaks to test app.

--- test-apps/jvm-lib/src/main/kotlin/sample/system/SystemInfo.kt
+++ test-apps/jvm-lib/src/main/kotlin/sample/system/SystemInfo.kt
@@ -1,8 +1,19 @@
 package sample.system
 
+data class SystemInfo(val kotlinVersion: String, val jvm: String, val os: String)
+
 fun reportSystemInfo() {
-    println("Kotlin: ${KotlinVersion.CURRENT}")
-    println("JVM: ${System.getProperty("java.version")} (${System.getProperty("java.vendor")})")
-    println("OS: ${System.getProperty("os.name")} (${System.getProperty("os.version")} ${System.getProperty("os.arch")})")
+    val systemInfo = getSystemInfo()
+    println("Kotlin: ${systemInfo.kotlinVersion}")
+    println("JVM: ${systemInfo.jvm}")
+    println("OS: ${systemInfo.os}")
     println()
+}
+
+fun getSystemInfo(): SystemInfo {
+    return SystemInfo(
+        KotlinVersion.CURRENT.toString(),
+        "${System.getProperty("java.version")} (${System.getProperty("java.vendor")})",
+        "${System.getProperty("os.name")} (${System.getProperty("os.version")} ${System.getProperty("os.arch")})"
+    )
 }
--- test-apps/jvm-ui-app/src/main/kotlin/sample/Main.kt
+++ test-apps/jvm-ui-app/src/main/kotlin/sample/Main.kt
@@ -3,18 +3,20 @@ package sample
 import sample.calc.Failure
 import sample.calc.Parser
 import sample.calc.Success
+import sample.system.getSystemInfo
 import java.awt.Color
 import java.awt.Dimension
 import java.awt.GraphicsEnvironment
 import java.awt.GridBagConstraints
 import java.awt.GridBagLayout
+import java.awt.Insets
 import javax.swing.*
 import javax.swing.event.DocumentEvent
 import javax.swing.event.DocumentListener
 
 fun main() {
     val frame = JFrame("Test App")
-    frame.size = Dimension(500, 400)
+    frame.size = Dimension(600, 500)
     frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
 
     val main = JPanel()
@@ -24,26 +26,35 @@ fun main() {
     main.border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
     frame.contentPane.add(main)
 
+    val labelInsets = Insets(10, 20, 10, 20)
+
     val input = JTextField()
     main.add(JLabel("Input:"), GridBagConstraints().also {
         it.gridx = 0
         it.gridy = 0
+        it.anchor = GridBagConstraints.BASELINE_TRAILING
+        it.insets = labelInsets
     })
     main.add(input, GridBagConstraints().also {
         it.gridx = 1
         it.gridy = 0
         it.fill = GridBagConstraints.HORIZONTAL
+        it.anchor = GridBagConstraints.BASELINE_LEADING
         it.weightx = 1.0
     })
 
     val expressionLabel = JLabel("")
+    val originalColor = expressionLabel.foreground
     main.add(JLabel("Expression:"), GridBagConstraints().also {
         it.gridx = 0
         it.gridy = 1
+        it.anchor = GridBagConstraints.BASELINE_TRAILING
+        it.insets = labelInsets
     })
     main.add(expressionLabel, GridBagConstraints().also {
         it.gridx = 1
         it.gridy = 1
+        it.anchor = GridBagConstraints.BASELINE_LEADING
         it.weightx = 1.0
     })
 
@@ -51,13 +62,39 @@ fun main() {
     main.add(JLabel("Result:"), GridBagConstraints().also {
         it.gridx = 0
         it.gridy = 2
-        it.weighty = 1.0
+        it.anchor = GridBagConstraints.BASELINE_TRAILING
+        it.insets = labelInsets
     })
     main.add(resultLabel, GridBagConstraints().also {
         it.gridx = 1
         it.gridy = 2
+        it.anchor = GridBagConstraints.BASELINE_LEADING
         it.weightx = 1.0
+    })
+
+    main.add(JLabel(), GridBagConstraints().also {
+        it.gridx = 0
+        it.gridy = 3
+        it.gridwidth = 2
+        it.anchor = GridBagConstraints.BASELINE_TRAILING
+        it.insets = labelInsets
         it.weighty = 1.0
+    })
+
+    main.add(JLabel("Kotlin:"), GridBagConstraints().also {
+        it.gridx = 0
+        it.gridy = 4
+        it.anchor = GridBagConstraints.BASELINE_TRAILING
+        it.insets = labelInsets
+    })
+
+    val systemInfo = getSystemInfo()
+    val systemInfoLabel = JLabel(systemInfo.kotlinVersion)
+    main.add(systemInfoLabel, GridBagConstraints().also {
+        it.gridx = 1
+        it.gridy = 4
+        it.anchor = GridBagConstraints.BASELINE_LEADING
+        it.weightx = 1.0
     })
 
     input.document.addDocumentListener(object : DocumentListener {
@@ -78,11 +115,13 @@ fun main() {
             val result = parser.parse(arrayOf(input.text))
             when (result) {
                 is Success -> {
+                    expressionLabel.foreground = originalColor
                     expressionLabel.text = result.expression.toString()
                     resultLabel.text = result.expression.evaluate().toString()
                 }
 
                 is Failure -> {
+                    expressionLabel.foreground = Color.red
                     expressionLabel.text = result.message
                     resultLabel.text = ""
                 }

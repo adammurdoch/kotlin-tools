42e32a8e3d334eaaaa8f8de93a7efbc59728e96b
Adam Murdoch <a@rubygrapefruit.net>
2025-02-09 05:51:01 +1100

    Add some explicit state management to `Action`.

--- libs/cli-args/src/commonMain/kotlin/net/rubygrapefruit/cli/Action.kt
+++ libs/cli-args/src/commonMain/kotlin/net/rubygrapefruit/cli/Action.kt
@@ -6,10 +6,7 @@ import kotlin.reflect.KClass
  * An action that can be configured using command-line arguments.
  */
 open class Action {
-    private val options = mutableListOf<Named>()
-    private val markers = mutableListOf<String>()
-    private val positional = mutableListOf<Positional>()
-    private val recoverables = mutableListOf<Recoverable>()
+    private var state: State = Buildable()
 
     /**
      * Allows parameters of type [String] to be added to this action.
@@ -104,8 +101,7 @@ open class Action {
         val actions = DefaultActions<T>(DefaultHost)
         builder(actions)
         val parameter = DefaultActionParameter(actions.build())
-        positional.add(parameter)
-        recoverables.addAll(parameter.recoverables)
+        state.addAction(parameter)
         return parameter
     }
 
@@ -129,204 +125,300 @@ open class Action {
      * Configures this object from the given arguments.
      */
     fun maybeParse(args: List<String>): Result {
-        val context = DefaultContext(DefaultHost, positional)
+        val context = DefaultContext(DefaultHost, state.positional)
         return maybeParse(args, context)
     }
 
     internal fun state(context: ParseContext): ParseState {
-        return ActionParseState(context, options, positional)
+        return state.parseState(context)
     }
 
     internal fun maybeParse(args: List<String>, parent: ParseContext): Result {
-        val context = parent.withOptions(options)
-        val actions = mutableListOf<() -> Unit>()
-        val hints = mutableListOf<FailureHint>()
-
-        var state = state(context)
-        var index = 0
-        while (index < args.size) {
-            val current = args.subList(index, args.size)
-            val result = state.parseNextValue(current)
-            when (result) {
-                is ParseState.Success -> {
-                    result.collect(actions, hints)
-                    var consumed = index + result.consumed
-                    return if (consumed == args.size) {
-                        actions.run()
-                        Result.Success
-                    } else {
-                        // Should not need this but currently required by Recoverable
-                        actions.run()
-                        attemptToRecover(args, consumed, null, false, hints.merge(), DefaultHost, result.context ?: context)
-                    }
-                }
-
-                is ParseState.Failure -> {
-                    var recognized = index + result.recognized
-                    return attemptToRecover(args, recognized, result.exception, result.expectedMore, hints.merge(), DefaultHost, context)
-                }
-
-                is ParseState.Nothing -> {
-                    attemptToRecover(args, index, null, false, hints.merge(), DefaultHost, context)
-                }
-
-                is ParseState.Continue -> {
-                    result.collect(actions, hints)
-                    state = result.state
-                    index += result.consumed
-                }
-            }
-        }
-
-        val result = state.endOfInput()
-        return when (result) {
-            is ParseState.FinishSuccess -> {
-                actions.add(result.apply)
-                actions.run()
-                Result.Success
-            }
-
-            is ParseState.FinishFailure -> {
-                attemptToRecover(args, args.size, result.exception, true, hints.merge(), DefaultHost, context)
-            }
-        }
-    }
-
-    private fun ParseState.Continue.collect(actions: MutableList<() -> Unit>, hints: MutableList<FailureHint>) {
-        actions.add(apply)
-        if (hint != null) {
-            hints.add(hint)
-        }
-    }
-
-    private fun ParseState.Success.collect(actions: MutableList<() -> Unit>, hints: MutableList<FailureHint>) {
-        actions.add(apply)
-        if (hint != null) {
-            hints.add(hint)
-        }
-    }
-
-    private fun List<() -> Unit>.run() {
-        forEach { it() }
-    }
-
-    private fun List<FailureHint>.merge(): FailureHint? {
-        return if (isEmpty()) {
-            null
-        } else if (size == 1) {
-            first()
-        } else {
-            val hints = toList()
-            object : FailureHint {
-                override fun map(args: List<String>): ParseState.Failure? {
-                    for (hint in hints) {
-                        val failure = hint.map(args)
-                        if (failure != null) {
-                            return failure
-                        }
-                    }
-                    return null
-                }
-            }
-        }
-    }
-
-    private fun attemptToRecover(
-        args: List<String>,
-        recognized: Int,
-        failure: ArgParseException?,
-        expectedMore: Boolean,
-        hint: FailureHint?,
-        host: Host,
-        parent: ParseContext
-    ): Result {
-        val context = parent.withOptions(options)
-
-        // Allow an option to recover from parse failure by attempting to do a "recovery" parse from where parsing stopped
-        var index = recognized
-        while (index in args.indices) {
-            val current = args.subList(index, args.size)
-            for (option in recoverables) {
-                val result = option.maybeRecover(current, context)
-                if (result) {
-                    // Don't attempt to keep parsing
-                    return Result.Success
-                }
-            }
-            // Skip this argument and attempt to recover on next argument
-            index++
-        }
-
-        if (hint != null && recognized < args.size) {
-            val failure = hint.map(args.subList(recognized, args.size))
-            if (failure != null) {
-                return Result.Failure(failure.exception)
-            }
-        }
-
-        // Determine the parse failure to report
-        val arg = args.getOrNull(recognized)
-        val originalFailure = failure
-        val failure = when {
-            // Parsing stopped on an unknown option
-            arg != null && host.isMarker(arg) -> {
-                // Throw away the failure if parsing expected more but stopped on an unknown option
-                val failure = if (originalFailure != null && expectedMore && !options.any { it.markers.contains(arg) }) {
-                    null
-                } else {
-                    originalFailure
-                }
-                failure ?: ArgParseException("Unknown option: $arg")
-            }
-
-            // Parsing stopped due to a failure
-            originalFailure != null -> originalFailure
-
-            // Parsing stopped on a positional parameter
-            else -> PositionalParseException("Unknown parameter: $arg", resolution = "Too many arguments provided: $arg", positional = context.positional)
-        }
-        return Result.Failure(failure)
+        val result = state.maybeParse(args, parent)
+        state = Finished(state.positional, state.usage())
+        return result
     }
 
     fun usage(): ActionUsage {
-        return ActionUsage(
-            options.flatMap { it.usage() },
-            positional.map { it.usage() }
-        )
+        return state.usage()
     }
 
     fun usage(name: String): ActionUsage? {
-        return positional.firstOrNull()?.usage(name)
+        return state.positional.firstOrNull()?.usage(name)
     }
 
     internal fun <T : Named> add(param: T): T {
-        val newMarkers = param.markers
-        for (marker in newMarkers) {
-            if (markers.contains(marker)) {
-                throw IllegalArgumentException("$marker is used by another option")
-            }
-        }
-        markers.addAll(newMarkers)
-        options.add(param)
+        state.addNamed(param)
         return param
     }
 
     internal fun <T : Positional> add(param: T): T {
-        positional.add(param)
+        state.addPositional(param)
         return param
     }
 
     internal fun <T : Named> replace(param: Named, newParam: T): T {
-        options[options.indexOf(param)] = newParam
+        state.replaceNamed(param, newParam)
         return newParam
     }
 
     internal fun <T : Positional> replace(param: Positional, newParam: T): T {
-        positional[positional.indexOf(param)] = newParam
+        state.replacePositional(param, newParam)
         return newParam
     }
 
-    internal fun positional(): List<Positional> = positional
+    internal fun positional(): List<Positional> = state.positional
+
+    private interface State {
+        val positional: List<Positional>
+
+        fun addNamed(param: Named)
+        fun replaceNamed(param: Named, newParam: Named)
+        fun addPositional(param: Positional)
+        fun replacePositional(param: Positional, newParam: Positional)
+        fun addAction(parameter: DefaultActionParameter<*>)
+
+        fun usage(): ActionUsage
+
+        fun parseState(context: ParseContext): ParseState
+
+        fun maybeParse(args: List<String>, parentContext: ParseContext): Result
+    }
+
+    private class Buildable : State {
+        private val options = mutableListOf<Named>()
+        private val markers = mutableListOf<String>()
+        override val positional = mutableListOf<Positional>()
+        private val recoverables = mutableListOf<Recoverable>()
+
+        override fun addNamed(param: Named) {
+            val newMarkers = param.markers
+            for (marker in newMarkers) {
+                if (markers.contains(marker)) {
+                    throw IllegalArgumentException("$marker is used by another option")
+                }
+            }
+            markers.addAll(newMarkers)
+            options.add(param)
+        }
+
+        override fun replaceNamed(param: Named, newParam: Named) {
+            options[options.indexOf(param)] = newParam
+        }
+
+        override fun addPositional(param: Positional) {
+            positional.add(param)
+        }
+
+        override fun replacePositional(param: Positional, newParam: Positional) {
+            positional[positional.indexOf(param)] = newParam
+        }
+
+        override fun addAction(parameter: DefaultActionParameter<*>) {
+            positional.add(parameter)
+            recoverables.addAll(parameter.recoverables)
+        }
+
+        override fun usage(): ActionUsage {
+            return ActionUsage(
+                options.flatMap { it.usage() },
+                positional.map { it.usage() }
+            )
+        }
+
+        override fun parseState(context: ParseContext): ParseState {
+            return ActionParseState(context, options, positional)
+        }
+
+        override fun maybeParse(args: List<String>, parent: ParseContext): Result {
+            val context = parent.withOptions(options)
+            val actions = mutableListOf<() -> Unit>()
+            val hints = mutableListOf<FailureHint>()
+
+            var state = parseState(context)
+            var index = 0
+            while (index < args.size) {
+                val current = args.subList(index, args.size)
+                val result = state.parseNextValue(current)
+                when (result) {
+                    is ParseState.Success -> {
+                        result.collect(actions, hints)
+                        var consumed = index + result.consumed
+                        return if (consumed == args.size) {
+                            actions.run()
+                            Result.Success
+                        } else {
+                            // Should not need this but currently required by Recoverable
+                            actions.run()
+                            attemptToRecover(args, consumed, null, false, hints.merge(), DefaultHost, result.context ?: context)
+                        }
+                    }
+
+                    is ParseState.Failure -> {
+                        var recognized = index + result.recognized
+                        return attemptToRecover(args, recognized, result.exception, result.expectedMore, hints.merge(), DefaultHost, context)
+                    }
+
+                    is ParseState.Nothing -> {
+                        attemptToRecover(args, index, null, false, hints.merge(), DefaultHost, context)
+                    }
+
+                    is ParseState.Continue -> {
+                        result.collect(actions, hints)
+                        state = result.state
+                        index += result.consumed
+                    }
+                }
+            }
+
+            val result = state.endOfInput()
+            return when (result) {
+                is ParseState.FinishSuccess -> {
+                    actions.add(result.apply)
+                    actions.run()
+                    Result.Success
+                }
+
+                is ParseState.FinishFailure -> {
+                    attemptToRecover(args, args.size, result.exception, true, hints.merge(), DefaultHost, context)
+                }
+            }
+        }
+
+        private fun attemptToRecover(
+            args: List<String>,
+            recognized: Int,
+            failure: ArgParseException?,
+            expectedMore: Boolean,
+            hint: FailureHint?,
+            host: Host,
+            parent: ParseContext
+        ): Result {
+            val context = parent.withOptions(options)
+
+            // Allow an option to recover from parse failure by attempting to do a "recovery" parse from where parsing stopped
+            var index = recognized
+            while (index in args.indices) {
+                val current = args.subList(index, args.size)
+                for (option in recoverables) {
+                    val result = option.maybeRecover(current, context)
+                    if (result) {
+                        // Don't attempt to keep parsing
+                        return Result.Success
+                    }
+                }
+                // Skip this argument and attempt to recover on next argument
+                index++
+            }
+
+            if (hint != null && recognized < args.size) {
+                val failure = hint.map(args.subList(recognized, args.size))
+                if (failure != null) {
+                    return Result.Failure(failure.exception)
+                }
+            }
+
+            // Determine the parse failure to report
+            val arg = args.getOrNull(recognized)
+            val originalFailure = failure
+            val failure = when {
+                // Parsing stopped on an unknown option
+                arg != null && host.isMarker(arg) -> {
+                    // Throw away the failure if parsing expected more but stopped on an unknown option
+                    val failure = if (originalFailure != null && expectedMore && !options.any { it.markers.contains(arg) }) {
+                        null
+                    } else {
+                        originalFailure
+                    }
+                    failure ?: ArgParseException("Unknown option: $arg")
+                }
+
+                // Parsing stopped due to a failure
+                originalFailure != null -> originalFailure
+
+                // Parsing stopped on a positional parameter
+                else -> PositionalParseException("Unknown parameter: $arg", resolution = "Too many arguments provided: $arg", positional = context.positional)
+            }
+            return Result.Failure(failure)
+        }
+
+
+        private fun ParseState.Continue.collect(actions: MutableList<() -> Unit>, hints: MutableList<FailureHint>) {
+            actions.add(apply)
+            if (hint != null) {
+                hints.add(hint)
+            }
+        }
+
+        private fun ParseState.Success.collect(actions: MutableList<() -> Unit>, hints: MutableList<FailureHint>) {
+            actions.add(apply)
+            if (hint != null) {
+                hints.add(hint)
+            }
+        }
+
+        private fun List<() -> Unit>.run() {
+            forEach { it() }
+        }
+
+        private fun List<FailureHint>.merge(): FailureHint? {
+            return if (isEmpty()) {
+                null
+            } else if (size == 1) {
+                first()
+            } else {
+                val hints = toList()
+                object : FailureHint {
+                    override fun map(args: List<String>): ParseState.Failure? {
+                        for (hint in hints) {
+                            val failure = hint.map(args)
+                            if (failure != null) {
+                                return failure
+                            }
+                        }
+                        return null
+                    }
+                }
+            }
+        }
+    }
+
+    private class Finished(
+        override val positional: List<Positional>,
+        val usage: ActionUsage
+    ) : State {
+        override fun usage(): ActionUsage {
+            return usage
+        }
+
+        override fun addNamed(param: Named) {
+            throw IllegalStateException()
+        }
+
+        override fun replaceNamed(param: Named, newParam: Named) {
+            throw IllegalStateException()
+        }
+
+        override fun addPositional(param: Positional) {
+            throw IllegalStateException()
+        }
+
+        override fun replacePositional(param: Positional, newParam: Positional) {
+            throw IllegalStateException()
+        }
+
+        override fun addAction(parameter: DefaultActionParameter<*>) {
+            throw IllegalStateException()
+        }
+
+        override fun parseState(context: ParseContext): ParseState {
+            throw IllegalStateException()
+        }
+
+        override fun maybeParse(args: List<String>, parentContext: ParseContext): Result {
+            throw IllegalStateException()
+        }
+    }
 
     sealed class Result {
         data object Success : Result()

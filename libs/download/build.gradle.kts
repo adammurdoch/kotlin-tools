070d1c59c3e39685f9aa62f070056184e51d69c6
Adam Murdoch <a@rubygrapefruit.net>
2026-01-04 16:55:53 +1100

    Fix module dependencies for lib download.

--- libs/download/build.gradle.kts
+++ libs/download/build.gradle.kts
@@ -6,8 +6,6 @@ group = versions.libs.group
 
 library {
     module.name = "net.rubygrapefruit.tools.download"
-    // TODO - this should not be required
-    module.requires.add("machine.info")
     targetJvmVersion = 11
     dependencies {
         implementation(versions.libs.coordinates("machine-info"))

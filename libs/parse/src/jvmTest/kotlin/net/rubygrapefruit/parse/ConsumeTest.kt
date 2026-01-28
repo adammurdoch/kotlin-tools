943add80f592dd1ec22b5dddaf6e19afd0c3dbf5
Adam Murdoch <a@rubygrapefruit.net>
2026-01-29 09:32:38 +1100

    Add test coverage.

--- libs/parse/src/jvmTest/kotlin/net/rubygrapefruit/parse/ConsumeTest.kt
+++ libs/parse/src/jvmTest/kotlin/net/rubygrapefruit/parse/ConsumeTest.kt
@@ -3,18 +3,46 @@ package net.rubygrapefruit.parse
 import net.rubygrapefruit.parse.combinators.consume
 import net.rubygrapefruit.parse.text.literal
 import net.rubygrapefruit.parse.text.parse
+import net.rubygrapefruit.parse.text.pushParser
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertIs
 
 class ConsumeTest : AbstractParseTest() {
     @Test
     fun `calls function with result of literal`() {
-        var result: Int? = 0
+        var counter = 0
         val parser = consume(literal("abc", 1)) {
-            result = it
+            counter++
         }
 
-        parser.parse("abc")
-        assertEquals(1, result)
+        parser.expecting {
+            expectMap {
+                expectLiteral("abc", result = 1)
+            }
+        }
+
+        val result = parser.parse("abc")
+        assertIs<ParseResult.Success<*>>(result)
+        assertEquals(1, counter)
+    }
+
+    @Test
+    fun `calls function when matching input becomes available`() {
+        var counter = 0
+        val parser = consume(literal("abc", 1)) {
+            counter++
+        }
+
+        val pushParser = parser.pushParser()
+        pushParser.input("ab".toCharArray())
+        assertEquals(0, counter)
+
+        pushParser.input("c".toCharArray())
+        assertEquals(1, counter)
+
+        val result = pushParser.endOfInput()
+        assertIs<ParseResult.Success<*>>(result)
+        assertEquals(1, counter)
     }
 }
--- libs/parse/src/jvmTest/kotlin/net/rubygrapefruit/parse/ZeroOrMoreOfConsumeTest.kt
+++ libs/parse/src/jvmTest/kotlin/net/rubygrapefruit/parse/ZeroOrMoreOfConsumeTest.kt
@@ -7,26 +7,40 @@ import net.rubygrapefruit.parse.combinators.consume
 import net.rubygrapefruit.parse.combinators.zeroOrMore
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertIs
 
-class ZeroOrMoreOfConsume : AbstractParseTest() {
+class ZeroOrMoreOfConsumeTest : AbstractParseTest() {
     @Test
     fun `matches zero or more literals`() {
         var counter = 0
-
         val parser = zeroOrMore(
             consume(literal(byteArrayOf(0x1, 0x2), 1)) {
                 counter++
             }
         )
 
-        parser.parse(byteArrayOf(0x1, 0x2, 0x1, 0x2))
+        val result = parser.parse(byteArrayOf(0x1, 0x2, 0x1, 0x2))
+        assertIs<ParseResult.Success<*>>(result)
         assertEquals(2, counter)
     }
 
     @Test
-    fun `calls function as input becomes available`() {
+    fun `calls function even when subsequent input fails`() {
         var counter = 0
+        val parser = zeroOrMore(
+            consume(literal(byteArrayOf(0x1, 0x2), 1)) {
+                counter++
+            }
+        )
 
+        val result = parser.parse(byteArrayOf(0x1, 0x2, 0x1, 0x3))
+        assertIs<ParseResult.Fail<*>>(result)
+        assertEquals(1, counter)
+    }
+
+    @Test
+    fun `calls function when matching input becomes available`() {
+        var counter = 0
         val parser = zeroOrMore(
             consume(literal(byteArrayOf(0x1, 0x2), 1)) {
                 counter++

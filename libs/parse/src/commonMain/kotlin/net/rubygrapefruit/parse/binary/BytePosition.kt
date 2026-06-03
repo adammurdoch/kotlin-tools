13e4124827f93866fdc732d04833b2dc398d0f83
Adam Murdoch <a@rubygrapefruit.net>
2026-06-04 09:23:29 +1000

    Add comments.

--- libs/parse/src/commonMain/kotlin/net/rubygrapefruit/parse/binary/BytePosition.kt
+++ libs/parse/src/commonMain/kotlin/net/rubygrapefruit/parse/binary/BytePosition.kt
@@ -3,6 +3,9 @@ package net.rubygrapefruit.parse.binary
 import net.rubygrapefruit.parse.Position
 import kotlin.jvm.JvmInline
 
+/**
+ * The position of a specific byte in a binary stream.
+ */
 @JvmInline
 value class BytePosition(val offset: Position) {
     override fun toString(): String {
--- libs/parse/src/commonMain/kotlin/net/rubygrapefruit/parse/text/CharPosition.kt
+++ libs/parse/src/commonMain/kotlin/net/rubygrapefruit/parse/text/CharPosition.kt
@@ -2,6 +2,9 @@ package net.rubygrapefruit.parse.text
 
 import net.rubygrapefruit.parse.Position
 
+/**
+ * The position of a specific character in a text stream.
+ */
 class CharPosition(val offset: Position, val line: Int, val col: Int) {
     override fun toString(): String {
         return "offset: $offset"

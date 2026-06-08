a080fcc89d71b7d8141d6a367d8bbf4f90e302a5
Adam Murdoch <a@rubygrapefruit.net>
2026-06-08 15:24:13 +1000

    Add test coverage.

---
+++ libs/parse/src/jvmTest/kotlin/net/rubygrapefruit/parse/ZeroOrMoreOfOneByteTest.kt
@@ -0,0 +1,37 @@
+package net.rubygrapefruit.parse
+
+import net.rubygrapefruit.parse.combinators.zeroOrMore
+import net.rubygrapefruit.parse.binary.one
+import kotlin.test.Test
+
+class ZeroOrMoreOfOneByteTest : AbstractParseTest() {
+    @Test
+    fun `matches zero or more of one byte`() {
+        val parser = zeroOrMore(
+            one()
+        )
+
+        parser.expecting {
+            expectZeroOrMoreSingleInput {
+                expectOneByte()
+            }
+        }
+
+        parser.matches(expected = emptyList()) {
+            steps {
+                advance(0)
+            }
+        }
+        parser.matches(0x1, expected = bytes(0x1)) {
+            steps {
+                advance(1)
+            }
+        }
+        parser.matches(0x5, expected = bytes(0x5))
+        parser.matches(0x5, 0x1, 0x1, expected = bytes(0x5, 0x1, 0x1)) {
+            steps {
+                advance(3)
+            }
+        }
+    }
+}

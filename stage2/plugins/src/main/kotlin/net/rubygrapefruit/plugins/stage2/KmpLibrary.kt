03bbb8ef1e803b169d4e640cc0e727a2e070a9fd
Adam Murdoch <a@rubygrapefruit.net>
2026-07-05 09:42:37 +1000

    Update stage3.

--- stage2/plugins/src/main/kotlin/net/rubygrapefruit/plugins/stage2/KmpLibrary.kt
+++ stage2/plugins/src/main/kotlin/net/rubygrapefruit/plugins/stage2/KmpLibrary.kt
@@ -4,4 +4,12 @@ abstract class KmpLibrary(val jvm: JvmLibrary) {
     fun jvm(config: JvmLibrary.() -> Unit) {
         jvm.config()
     }
+
+    fun nativeDesktop() {
+        // Ignore
+    }
+
+    fun browser() {
+        // Ignore
+    }
 }

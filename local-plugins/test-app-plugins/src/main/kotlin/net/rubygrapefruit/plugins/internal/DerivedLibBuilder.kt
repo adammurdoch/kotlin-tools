69714c6445f0eb63c5521e0543ef9ec00a21f263
Adam Murdoch <a@rubygrapefruit.net>
2026-07-19 06:26:28 +1000

    Refactor sample verification.

--- local-plugins/test-app-plugins/src/main/kotlin/net/rubygrapefruit/plugins/internal/DerivedLibBuilder.kt
+++ local-plugins/test-app-plugins/src/main/kotlin/net/rubygrapefruit/plugins/internal/DerivedLibBuilder.kt
@@ -1,19 +1,59 @@
 package net.rubygrapefruit.plugins.internal
 
-class DerivedJvmLibBuilder(val name: String) {
-    internal var jvmVersion = 17
-        private set
+import net.rubygrapefruit.plugins.stage0.BuildConstants
+
+class DerivedJvmLibBuilder internal constructor(
+    val name: String,
+    private val container: SampleContainer
+) {
+    private var jvmVersion = BuildConstants.constants.libs.jvm.version
 
     fun requiresJvm(version: Int) {
         jvmVersion = version
     }
+
+    internal fun register(derivedFrom: SourceTree?): JvmLib {
+        return container.add(name) { name, sampleDir ->
+            val sourceDir = derivedFrom.generatedInto(sampleDir, "src/main", "src/test")
+            JvmLib(name, sourceDir, jvmVersion)
+        }
+    }
 }
 
-class DerivedKmpLibBuilder(val name: String) {
-    internal var jvmVersion = 17
-        private set
+class DerivedKmpLibBuilder internal constructor(
+    val name: String,
+    private val container: SampleContainer
+) {
+    private var jvm = true
+    private var jvmVersion = BuildConstants.constants.libs.jvm.version
+
+    fun noJvm() {
+        jvm = false
+    }
 
     fun requiresJvm(version: Int) {
         jvmVersion = version
+    }
+
+    internal fun register(derivedFrom: SourceTree?): KmpLib {
+        return container.add(name) { name, sampleDir ->
+            val sourceTree = if (derivedFrom == null) {
+                OriginSourceTree(
+                    sampleDir,
+                    mainPath = "src/commonMain",
+                    testPath = "src/commonTest",
+                    listOf(
+                        "src/desktopMain",
+                        "src/jvmMain",
+                        "src/mingwMain",
+                        "src/unixMain",
+                        "src/jsMain"
+                    )
+                )
+            } else {
+                derivedFrom.generatedInto(sampleDir)
+            }
+            KmpLib(name, sourceTree, if (jvm) jvmVersion else null)
+        }
     }
 }
--- local-plugins/test-app-plugins/src/main/kotlin/net/rubygrapefruit/plugins/internal/LibBuilder.kt
+++ local-plugins/test-app-plugins/src/main/kotlin/net/rubygrapefruit/plugins/internal/LibBuilder.kt
@@ -1,84 +1,56 @@
 package net.rubygrapefruit.plugins.internal
 
-import java.nio.file.Path
-
 sealed class LibBuilder {
     protected var sourceDirs = true
 
     fun noSourceDirs() {
         sourceDirs = false
     }
-
-    protected fun sourceTree(sampleDir: Path, main: String, test: String, vararg additionalPaths: String): SourceTree {
-        return if (sourceDirs) {
-            OriginSourceTree(sampleDir, main, test, additionalPaths.toList())
-        } else {
-            NoSourceDirs(sampleDir)
-        }
-    }
 }
 
 class JvmLibBuilder internal constructor(
-    private val name: String,
+    name: String,
     private val container: SampleContainer
 ) : LibBuilder() {
     private val derived = mutableListOf<DerivedJvmLibBuilder>()
+    private val builder = DerivedJvmLibBuilder(name, container)
 
     fun derive(name: String, config: DerivedJvmLibBuilder.() -> Unit = {}) {
-        val builder = DerivedJvmLibBuilder(name)
+        val builder = DerivedJvmLibBuilder(name, container)
         builder.config()
         derived.add(builder)
     }
 
     internal fun register(): JvmLib {
-        val lib = container.add(name) { name, sampleDir ->
-            val sourceTree = sourceTree(sampleDir, main = "src/main", test = "src/test")
-            JvmLib(name, sourceTree, 17)
-        }
+        val lib = builder.register(null)
         for (builder in derived) {
-            container.add(builder.name) { name, sampleDir ->
-                JvmLib(name, lib.sourceTree.generatedInto(sampleDir), builder.jvmVersion)
-            }
+            builder.register(lib.sourceTree)
         }
         return lib
     }
 }
 
 class KmpLibBuilder internal constructor(
-    private val name: String,
+    name: String,
     private val container: SampleContainer
 ) : LibBuilder() {
-    private var jvm = true
     private val derived = mutableListOf<DerivedKmpLibBuilder>()
+    private val builder = DerivedKmpLibBuilder(name, container)
 
     fun noJvm() {
-        jvm = false
+        builder.noJvm()
     }
 
     fun derive(name: String, config: DerivedKmpLibBuilder.() -> Unit = {}) {
-        val builder = DerivedKmpLibBuilder(name)
+        val builder = DerivedKmpLibBuilder(name, container)
         builder.config()
         derived.add(builder)
     }
 
     internal fun register(): KmpLib {
-        val lib = container.add(name) { name, sampleDir ->
-            val sourceTree = sourceTree(
-                sampleDir,
-                main = "src/commonMain",
-                test = "src/commonTest",
-                "src/desktopMain",
-                "src/jvmMain",
-                "src/mingwMain",
-                "src/unixMain",
-                "src/jsMain",
-            )
-            KmpLib(name, sourceTree, if (jvm) 17 else null)
-        }
+        val lib = builder.register(null)
         for (builder in derived) {
-            container.add(builder.name) { name, sampleDir ->
-                KmpLib(name, lib.sourceTree.generatedInto(sampleDir), builder.jvmVersion)
-            }
+            builder.register(lib.sourceTree)
         }
         return lib
     }

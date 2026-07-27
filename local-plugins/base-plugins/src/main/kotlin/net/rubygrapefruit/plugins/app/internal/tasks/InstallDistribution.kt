aca9956f4738a6f204a7fff056e83a8b7c0fdf81
Adam Murdoch <a@rubygrapefruit.net>
2026-07-28 07:55:47 +1000

    Rework install task.

--- local-plugins/base-plugins/src/main/kotlin/net/rubygrapefruit/plugins/app/internal/tasks/InstallDistribution.kt
+++ local-plugins/base-plugins/src/main/kotlin/net/rubygrapefruit/plugins/app/internal/tasks/InstallDistribution.kt
@@ -3,13 +3,14 @@ package net.rubygrapefruit.plugins.app.internal.tasks
 import net.rubygrapefruit.plugins.app.Distribution
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
-import org.gradle.api.file.FileSystemOperations
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.*
 import org.gradle.api.tasks.options.Option
-import java.nio.file.Files
-import javax.inject.Inject
+import java.io.IOException
+import java.nio.file.*
+import java.nio.file.attribute.BasicFileAttributes
+import kotlin.io.path.deleteIfExists
 
 abstract class InstallDistribution : DefaultTask() {
     @get:Option(option = "release", description = "Installs the release distribution")
@@ -42,9 +43,6 @@ abstract class InstallDistribution : DefaultTask() {
     @get:InputDirectory
     abstract val sourceImageDirectory: DirectoryProperty
 
-    @get:Inject
-    protected abstract val fileOperations: FileSystemOperations
-
     init {
         release.convention(false)
         sourceImageDirectory.set(project.provider {
@@ -60,10 +58,38 @@ abstract class InstallDistribution : DefaultTask() {
     fun install() {
         val srcDir = sourceImageDirectory.get().asFile.toPath()
         val targetDir = targetImageDirectory.get().asFile.toPath()
-        fileOperations.sync { spec ->
-            spec.from(srcDir)
-            spec.into(targetDir)
-        }
+
+        Files.walkFileTree(targetDir, object : SimpleFileVisitor<Path>() {
+            override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
+                file.deleteIfExists()
+                return FileVisitResult.CONTINUE
+            }
+
+            override fun postVisitDirectory(dir: Path, exc: IOException?): FileVisitResult {
+                dir.deleteIfExists()
+                return FileVisitResult.CONTINUE
+            }
+        })
+
+        Files.walkFileTree(srcDir, object : SimpleFileVisitor<Path>() {
+            override fun preVisitDirectory(dir: Path, attrs: BasicFileAttributes): FileVisitResult {
+                val targetDir = targetPath(dir)
+                Files.createDirectory(targetDir)
+                val permissions = Files.getPosixFilePermissions(dir)
+                Files.setPosixFilePermissions(targetDir, permissions)
+                return FileVisitResult.CONTINUE
+            }
+
+            override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
+                val targetFile = targetPath(file)
+                Files.copy(file, targetFile, LinkOption.NOFOLLOW_LINKS)
+                val permissions = Files.getPosixFilePermissions(file)
+                Files.setPosixFilePermissions(targetFile, permissions)
+                return FileVisitResult.CONTINUE
+            }
+
+            private fun targetPath(path: Path) = targetDir.resolve(srcDir.relativize(path))
+        })
 
         val link = targetLauncherLink.get().asFile.toPath()
         val linkDir = link.parent

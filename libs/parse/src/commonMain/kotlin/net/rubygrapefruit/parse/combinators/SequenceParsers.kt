69f95529912bfad52f3a9e37c5ef91bfdbb36862
Adam Murdoch <a@rubygrapefruit.net>
2026-08-02 06:09:51 +1000

    Refactor sequence parsers.

--- libs/parse/src/commonMain/kotlin/net/rubygrapefruit/parse/combinators/SequenceParsers.kt
+++ libs/parse/src/commonMain/kotlin/net/rubygrapefruit/parse/combinators/SequenceParsers.kt
@@ -91,7 +91,7 @@ fun <IN> sequence(a: Parser<IN, Unit>, b: Parser<IN, Unit>, vararg additional: P
  * Uses the given mapping function to produce the result from the result of the parsers.
  */
 fun <IN, A, B, C, OUT> sequence(a: Parser<IN, A>, b: Parser<IN, B>, c: Parser<IN, C>, map: (A, B, C) -> OUT): Parser<IN, OUT> {
-    return Sequence2Parser(a, seq2(b, c)) { a, tail -> map(a, tail.a, tail.b) }
+    return seq3(a, tuple2(b, c), map)
 }
 
 /**
@@ -108,7 +108,7 @@ fun <IN, A, B, OUT> prefixed(prefix: Parser<IN, *>, a: Parser<IN, A>, b: Parser<
  */
 @JvmName("prefixSequence")
 fun <IN, A, B, OUT> sequence(prefix: Parser<IN, Unit>, a: Parser<IN, A>, b: Parser<IN, B>, map: (A, B) -> OUT): Parser<IN, OUT> {
-    return sequence(prefix, a, b) { _, a, b -> map(a, b) }
+    return sequence(prefix, sequence(a, b, map))
 }
 
 /**
@@ -125,7 +125,7 @@ fun <IN, OUT> quoted(prefix: Parser<IN, *>, parser: Parser<IN, OUT>, suffix: Par
  */
 @JvmName("quotedSequence")
 fun <IN, OUT> sequence(prefix: Parser<IN, Unit>, parser: Parser<IN, OUT>, suffix: Parser<IN, Unit>): Parser<IN, OUT> {
-    return sequence(discard(prefix), parser, discard(suffix)) { _, b, _ -> b }
+    return sequence(prefix, sequence(parser, suffix))
 }
 
 /**
@@ -134,7 +134,7 @@ fun <IN, OUT> sequence(prefix: Parser<IN, Unit>, parser: Parser<IN, OUT>, suffix
  */
 @JvmName("quotedSequence")
 fun <IN, INTERMEDIATE, OUT> sequence(prefix: Parser<IN, Unit>, parser: Parser<IN, INTERMEDIATE>, suffix: Parser<IN, Unit>, map: (INTERMEDIATE) -> OUT): Parser<IN, OUT> {
-    return sequence(discard(prefix), parser, discard(suffix)) { _, b, _ -> map(b) }
+    return sequence(prefix, sequence(parser, suffix) { b, _ -> map(b) })
 }
 
 /**
@@ -150,7 +150,7 @@ fun <IN, A, B, OUT> separated(a: Parser<IN, A>, separator: Parser<IN, *>, b: Par
  * Produces the result of applying the given mapping function to the result of the first and last parsers.
  */
 fun <IN, A, B, OUT> sequence(a: Parser<IN, A>, separator: Parser<IN, Unit>, b: Parser<IN, B>, map: (A, B) -> OUT): Parser<IN, OUT> {
-    return sequence(a, separator, b) { a, _, b -> map(a, b) }
+    return sequence(a, sequence(separator, b), map)
 }
 
 /*
@@ -168,7 +168,7 @@ fun <IN, A, B, C, D, OUT> sequence(
     d: Parser<IN, D>,
     map: (A, B, C, D) -> OUT
 ): Parser<IN, OUT> {
-    return Sequence2Parser(a, seq3(b, c, d)) { a, tail -> map(a, tail.a, tail.b, tail.c) }
+    return Sequence2Parser(a, tuple3(b, c, d)) { a, tail -> map(a, tail.a, tail.b, tail.c) }
 }
 
 /*
@@ -187,7 +187,7 @@ fun <IN, A, B, C, D, E, OUT> sequence(
     e: Parser<IN, E>,
     map: (A, B, C, D, E) -> OUT
 ): Parser<IN, OUT> {
-    return Sequence2Parser(a, seq4(b, c, d, e)) { a, tail -> map(a, tail.a, tail.b, tail.c, tail.d) }
+    return Sequence2Parser(a, tuple4(b, c, d, e)) { a, tail -> map(a, tail.a, tail.b, tail.c, tail.d) }
 }
 
 /**
@@ -217,7 +217,7 @@ fun <IN, A, B, C, OUT> sequence(
     c: Parser<IN, C>,
     map: (A, B, C) -> OUT
 ): Parser<IN, OUT> {
-    return sequence(a, separator1, b, separator2, c) { a, _, b, _, c -> map(a, b, c) }
+    return seq3(a, sequence(separator1, tuple2(b, sequence(separator2, c))), map)
 }
 
 /*
@@ -237,7 +237,7 @@ fun <IN, A, B, C, D, E, F, OUT> sequence(
     f: Parser<IN, F>,
     map: (A, B, C, D, E, F) -> OUT
 ): Parser<IN, OUT> {
-    return Sequence2Parser(a, seq5(b, c, d, e, f)) { a, tail -> map(a, tail.a, tail.b, tail.c, tail.d, tail.e) }
+    return Sequence2Parser(a, tuple5(b, c, d, e, f)) { a, tail -> map(a, tail.a, tail.b, tail.c, tail.d, tail.e) }
 }
 
 /*
@@ -258,29 +258,62 @@ fun <IN, A, B, C, D, E, F, G, OUT> sequence(
     g: Parser<IN, G>,
     map: (A, B, C, D, E, F, G) -> OUT
 ): Parser<IN, OUT> {
-    return Sequence2Parser(a, seq6(b, c, d, e, f, g)) { a, tail -> map(a, tail.a, tail.b, tail.c, tail.d, tail.e, tail.f) }
+    return Sequence2Parser(a, tuple6(b, c, d, e, f, g)) { a, tail -> map(a, tail.a, tail.b, tail.c, tail.d, tail.e, tail.f) }
 }
 
 /*
  * Tuple parsers.
  */
 
-private fun <IN, A, B> seq2(a: Parser<IN, A>, b: Parser<IN, B>): Parser<IN, Tuple2<A, B>> {
+private fun <IN, A, B> tuple2(
+    a: Parser<IN, A>,
+    b: Parser<IN, B>
+): Parser<IN, Tuple2<A, B>> {
     return Sequence2Parser(a, b) { a, b -> Tuple2(a, b) }
 }
 
-private fun <IN, A, B, C> seq3(a: Parser<IN, A>, b: Parser<IN, B>, c: Parser<IN, C>): Parser<IN, Tuple3<A, B, C>> {
-    return Sequence2Parser(a, seq2(b, c)) { a, tail -> Tuple3(a, tail) }
+private fun <IN, A, B, C> tuple3(
+    a: Parser<IN, A>,
+    b: Parser<IN, B>,
+    c: Parser<IN, C>
+): Parser<IN, Tuple3<A, B, C>> {
+    return Sequence2Parser(a, tuple2(b, c)) { a, tail -> Tuple3(a, tail) }
 }
 
-private fun <IN, A, B, C, D> seq4(a: Parser<IN, A>, b: Parser<IN, B>, c: Parser<IN, C>, d: Parser<IN, D>): Parser<IN, Tuple4<A, B, C, D>> {
-    return Sequence2Parser(a, seq3(b, c, d)) { a, tail -> Tuple4(a, tail) }
+private fun <IN, A, B, C, OUT> seq3(
+    a: Parser<IN, A>,
+    tail: Parser<IN, Tuple2<B, C>>,
+    map: (A, B, C) -> OUT
+): Parser<IN, OUT> {
+    return Sequence2Parser(a, tail) { a, tail -> map(a, tail.a, tail.b) }
 }
 
-private fun <IN, A, B, C, D, E> seq5(a: Parser<IN, A>, b: Parser<IN, B>, c: Parser<IN, C>, d: Parser<IN, D>, e: Parser<IN, E>): Parser<IN, Tuple5<A, B, C, D, E>> {
-    return Sequence2Parser(a, seq4(b, c, d, e)) { a, tail -> Tuple5(a, tail) }
+private fun <IN, A, B, C, D> tuple4(
+    a: Parser<IN, A>,
+    b: Parser<IN, B>,
+    c: Parser<IN, C>,
+    d: Parser<IN, D>
+): Parser<IN, Tuple4<A, B, C, D>> {
+    return Sequence2Parser(a, tuple3(b, c, d)) { a, tail -> Tuple4(a, tail) }
 }
 
-private fun <IN, A, B, C, D, E, F> seq6(a: Parser<IN, A>, b: Parser<IN, B>, c: Parser<IN, C>, d: Parser<IN, D>, e: Parser<IN, E>, f: Parser<IN, F>): Parser<IN, Tuple6<A, B, C, D, E, F>> {
-    return Sequence2Parser(a, seq5(b, c, d, e, f)) { a, tail -> Tuple6(a, tail) }
+private fun <IN, A, B, C, D, E> tuple5(
+    a: Parser<IN, A>,
+    b: Parser<IN, B>,
+    c: Parser<IN, C>,
+    d: Parser<IN, D>,
+    e: Parser<IN, E>
+): Parser<IN, Tuple5<A, B, C, D, E>> {
+    return Sequence2Parser(a, tuple4(b, c, d, e)) { a, tail -> Tuple5(a, tail) }
+}
+
+private fun <IN, A, B, C, D, E, F> tuple6(
+    a: Parser<IN, A>,
+    b: Parser<IN, B>,
+    c: Parser<IN, C>,
+    d: Parser<IN, D>,
+    e: Parser<IN, E>,
+    f: Parser<IN, F>
+): Parser<IN, Tuple6<A, B, C, D, E, F>> {
+    return Sequence2Parser(a, tuple5(b, c, d, e, f)) { a, tail -> Tuple6(a, tail) }
 }

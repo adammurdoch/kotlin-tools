df3b1e8b50e0886c787e09e30d763e8d763c1162
Adam Murdoch <a@rubygrapefruit.net>
2026-09-23 08:56:22 +1000

    Refactor JVM UI application for consistency with other JVM components.

--- local-plugins/launcher-plugins/src/main/kotlin/net/rubygrapefruit/plugins/app/internal/DefaultJvmUiApplication.kt
+++ local-plugins/launcher-plugins/src/main/kotlin/net/rubygrapefruit/plugins/app/internal/DefaultJvmUiApplication.kt
@@ -12,16 +12,13 @@ abstract class DefaultJvmUiApplication @Inject constructor(
     objects: ObjectFactory,
     providers: ProviderFactory,
     project: Project
-) : DefaultUiApplication(objects, providers, project), MutableJvmApplication, JvmUiApplication, HasDependencies, HasGeneratedSource, HasGeneratedResources, HasTests {
+) : DefaultUiApplication(objects, providers, project), MutableJvmApplication, JvmUiApplication, PlatformContribution, HasTests {
+    override val main = DefaultJvmSourceSet("main", generatedSource, generatedResources)
     override val test: HasDependencies = DefaultHasDependencies("test")
     override val runtimeModulePath: ConfigurableFileCollection = objects.fileCollection()
-    override val dependencies = DefaultDependencies()
-
-    override val sourceSetName: String
-        get() = "main"
 
     override fun dependencies(config: Dependencies.() -> Unit) {
-        dependencies.config()
+        main.dependencies.config()
     }
 
     override fun test(config: Dependencies.() -> Unit) {
